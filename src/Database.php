62a62298a2abbc149732c5018e776dddd24d38f4
Josh Moody <jgmoody@gmail.com>
2014-01-26 17:57:38 -0600

    Added wrapper for Illuminate\Database

---
+++ src/Database.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Toadsuck\Core;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Prelude\Dsn\DsnParser;
+
+class Database
+{
+	public static $driver = null;
+	
+	public static function init($config = null)
+	{
+		$defaults = [
+			'driver'	=> 'mysql',
+			'host'		=> 'localhost',
+			'database'	=> 'mysql',
+			'username'	=> 'root',
+			'password'	=> 'root',
+			'charset'	=> 'utf8',
+			'collation'	=> 'utf8_unicode_ci',
+			'prefix'	=> null
+		];
+		
+		$capsule = new Capsule;
+		
+		if (is_array($config)) {
+			$options = array_merge($defaults, $config);
+		} else {
+			$options = $defaults;
+		}
+		
+		$capsule->addConnection($options);
+		$capsule->setAsGlobal();
+		$capsule->bootEloquent();
+		
+		self::$driver = $options['driver'];
+	}
+	
+	public static function random()
+	{
+		if (self::$driver == 'sqlite') {
+			return 'random()';
+		} else {
+			return 'rand()';
+		}
+	}
+	
+	public static function parseDsn($string = null)
+	{
+		$opts = null;
+		
+		if (!empty($string)) {
+			$dsn = (object) DsnParser::parseUrl($string)->toArray();
+			
+			$opts = [
+				'driver'	=> $dsn->driver,
+				'host'		=> $dsn->host,
+				'database'	=> $dsn->dbname,
+				'username'	=> $dsn->user,
+				'password'	=> $dsn->pass
+			];
+		}
+
+		return $opts;
+	}
+}
