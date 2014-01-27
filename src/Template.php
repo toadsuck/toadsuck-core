7552a71243d327b15c0afc426f8dc93b6b6e2fa9
Josh Moody <jgmoody@gmail.com>
2014-01-26 23:54:55 -0600

    Auto-escape all variables before rendering.

--- src/Template.php
+++ src/Template.php
@@ -7,7 +7,6 @@ namespace Toadsuck\Core;
  */
 class Template extends \League\Plates\Template
 {
-	public $toadsuck_base_path = null;
 	public $engine;
 	
 	public function __construct($path = null)
@@ -30,9 +29,27 @@ class Template extends \League\Plates\Template
 	}
 	
 	/**
+	 * Sanitize the data before rendering.
+	 */
+	public function render($view, array $data = null)
+	{
+		// Sanitize variables already in the template.
+		foreach (get_object_vars($this) as $key => $value) {
+			if ($key != 'engine') {
+				$this->$key = $this->scrub($value);
+			}
+		}
+	
+		// Also sanitize any variables we are passing in to the tempalte
+		$data = $this->scrub($data);
+
+		return parent::render($view, $data);
+	}
+	
+	/**
 	 * Output the content instead of just render.
 	 */
-	public function output($view, $data = null)
+	public function output($view, array $data = null)
 	{
 		echo $this->render($view, $data);
 	}
@@ -40,5 +57,36 @@ class Template extends \League\Plates\Template
 	public function addFolder($name, $path)
 	{
 		$this->engine->addFolder($name, $path);
+	}
+
+	/**
+	 * Recursively sanitize output
+	 */
+	public function scrub($var)
+	{
+		if (is_string($var)) {
+			// Sanitize strings
+			return $this->escape($var);
+	
+		} elseif (is_array($var)) {
+			// Santize arrays.
+			while (list($key) = each($var)) {
+				$var[$key] = $this->scrub($var[$key]);
+			}
+	
+			return $var;
+		} elseif (is_object($var)) {
+			// Sanitize objects
+			$values = get_object_vars($var);
+	
+			foreach ($values as $key => $value) {
+				$var->$key = $this->scrub($value);
+			}
+			return $var;
+	
+		} else {
+			// Not sure what this is. null or bool? Just return it.
+			return $var;
+		}
 	}
 }
