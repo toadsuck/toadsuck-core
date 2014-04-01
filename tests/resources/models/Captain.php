e8b37b64b751ef3758a13c5695c217b0a67290f9
Josh Moody <jgmoody@gmail.com>
2014-03-31 23:34:59 -0500

    Make codesniffer happy.

--- tests/resources/models/Captain.php
+++ tests/resources/models/Captain.php
@@ -12,14 +12,12 @@ class Captain extends Model
 	{
 		$query = self::queryBuilder();
 		
-		if (array_key_exists('first_name', $params))
-		{
-			$query->where('first_name', 'like', $params['first_name'] . '%'); 
+		if (array_key_exists('first_name', $params)) {
+			$query->where('first_name', 'like', $params['first_name'] . '%');
 		}
 
-		if (array_key_exists('last_name', $params))
-		{
-			$query->where('last_name', 'like', $params['last_name'] . '%'); 
+		if (array_key_exists('last_name', $params)) {
+			$query->where('last_name', 'like', $params['last_name'] . '%');
 		}
 		
 		return $query->get();
