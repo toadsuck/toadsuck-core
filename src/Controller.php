f6d716175ca6e2520680c112e6ba6410a1901a4e
Josh Moody <jgmoody@gmail.com>
2014-04-08 21:32:22 -0500

    Session expiration.

--- src/Controller.php
+++ src/Controller.php
@@ -36,18 +36,45 @@ class Controller
 		
 		// Shortcuts to the request object for cleaner syntax.
 		$this->input = new Input($this->request);
+		
+		// Initialize the Session.
+		$this->initializeSession();
+	}
 
+	/**
+	 * Initialize the session.
+	 *
+	 * This is something you might want to override in your controller so you can
+	 * redirect to a page with a message about being logged out after detecting the session has expired.
+	 *
+	 */
+	protected function initializeSession($session_expiration = null)
+	{
 		// Setup the session
 		$this->session = new Session();
+
+		$this->config->load('config');
 		
 		// We need a unique session name for this app. Let's use last 10 characters the file path's sha1 hash.
 		try {
 			$this->session->setName('TSAPP' . substr(sha1(__FILE__), -10));
+			$this->session->start();
+
+			// Default session expiration 1 hour.
+			// Can be overridden in method param or by setting session_expiration in config.php
+			$session_expiration = !empty($session_expiration)
+										? $session_expiration
+										: $this->config->get('session_expiration', 3600);
+
+			// Is this session too old?
+			if (time() - $this->session->getMetadataBag()->getLastUsed() > $session_expiration) {
+				$this->session->invalidate();
+			}
 		} catch (\LogicException $e) {
 			// Session already active, can't change it now!
 		}
 	}
-
+	
 	/**
 	 * Internal or External Redirect to the specified url
 	 */
